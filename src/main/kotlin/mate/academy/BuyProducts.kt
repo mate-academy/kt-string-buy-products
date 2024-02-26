2a2071fba91e25986bd168f11d59e76e61cd6711
VolodymyrDavidov <volodymyrdavidov1@gmail.com>
2024-02-26 18:07:37 +0200

    done

--- src/main/kotlin/mate/academy/BuyProducts.kt
+++ src/main/kotlin/mate/academy/BuyProducts.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
+val client = "Mary"
+val price = 30
+val quantity = 3
+
 fun getTotalPriceMessage(client: String, price: Int, quantity: Int): String {
-    return ""
+    val cost = price * quantity;
+    return "$client has to pay \$$cost per $quantity products"
 }
