4227bba8c9e851d43d8de888d3be51ad566d182f
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2024-02-27 07:56:00 +0200

    first version2

--- src/main/kotlin/mate/academy/BuyProducts.kt
+++ src/main/kotlin/mate/academy/BuyProducts.kt
@@ -1,9 +1,7 @@
 package mate.academy
 
-fun main() {
-    getTotalPriceMessage("ffff", 20, 3)
-}
-
 fun getTotalPriceMessage(client: String, price: Int, quantity: Int): String {
+
     return "$client has to pay $${price * quantity} per $quantity products"
+
 }
