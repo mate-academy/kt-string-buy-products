70de1dde58fb7b3ba784758368872e500f9590e6
Denys <denys.shl@mail.com>
2024-04-19 14:17:40 +0300

    hw is done

--- src/main/kotlin/mate/academy/BuyProducts.kt
+++ src/main/kotlin/mate/academy/BuyProducts.kt
@@ -1,5 +1,16 @@
 package mate.academy
 
 fun getTotalPriceMessage(client: String, price: Int, quantity: Int): String {
-    return ""
+    return "${client} has to pay ${price * quantity} per $quantity products"
+}
+
+private const val PRICE = 30
+private const val QUANTITY = 3
+private const val CLIENT = "Mary"
+
+fun main() {
+    val client = CLIENT
+    val price = PRICE
+    val quantity = QUANTITY
+    println(getTotalPriceMessage(client, price, quantity))
 }
